51d20f1efa94ae3f2302c3f0cde2b0d21fcc3e8c
Matthew Hammond <ultramatt96@hotmail.co.uk>
2024-11-26 12:41:40 +0000

    feat: add enum to market queue type

--- src/types/market.ts
+++ src/types/market.ts
@@ -1,4 +1,13 @@
 import { PublicKey } from "@solana/web3.js";
+import { EnumValues } from "./utils.js";
+
+export const marketQueue = {
+  "JOB_QUEUE": 0,
+  "NODE_QUEUE": 1,
+  "EMPTY": undefined
+} as const
+
+export type MarketQueue = EnumValues<typeof marketQueue>
 
 export type Market = {
   address: PublicKey;
@@ -11,6 +20,6 @@ export type Market = {
   vaultBump: number;
   nodeAccessKey: PublicKey;
   nodeXnosMinimum: number;
-  queueType: number;
+  queueType: MarketQueue;
   queue: Array<PublicKey>;
 };
