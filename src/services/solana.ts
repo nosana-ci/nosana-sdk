44e0d750de77aa4c3a22828f91a6fca64e9e5fb1
KeesGeerligs <166137421+KeesGeerligs@users.noreply.github.com>
2025-02-07 12:32:25 +0100

    Source mint fix

--- src/services/solana.ts
+++ src/services/solana.ts
@@ -89,7 +89,7 @@ export class SolanaManager {
   config: SolanaConfig;
   wallet: AnchorWallet;
   connection: Connection | undefined;
-  private readonly SOURCE_MINTS = {
+  protected readonly SOURCE_MINTS = {
     SOL: 'So11111111111111111111111111111111111111112',
     USDC: 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v',
     USDT: 'Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB',
