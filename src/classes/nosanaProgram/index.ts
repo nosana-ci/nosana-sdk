ea9fed0ae1f02f01d9ad1d6dd2f36f233f382e59
Matthew Hammond <ultramatt96@hotmail.co.uk>
2025-02-10 06:45:47 +0000

    chore: support disable prio fees

--- src/classes/nosanaProgram/index.ts
+++ src/classes/nosanaProgram/index.ts
@@ -2,6 +2,8 @@ import { ConfirmOptions } from '@solana/web3.js';
 import { Idl, MethodsNamespace, Program } from '@coral-xyz/anchor';
 
 import { getPriorityFeePreInstruction } from './priorityFees/getPriorityFeePreInstruction.js';
+import { config } from 'process';
+import { option } from '@metaplex-foundation/umi/serializers.js';
 
 export class NosanaProgram<IDL extends Idl = Idl> extends Program<IDL> {
   // TODO: Some how correct the type to add the new optional args to methods RPC Fnc
@@ -15,22 +17,33 @@ export class NosanaProgram<IDL extends Idl = Idl> extends Program<IDL> {
   private prioFeeProxyMethods() {
     this.methods = new Proxy(this.methods, {
       get(target, prop) {
-        return function (...args: unknown[]) {
-          const method = target[prop as keyof typeof target](...args);
-          const methodProxy = new Proxy(method, {
-            get(target, prop) {
-              if (prop === 'rpc') {
-                return async function (options?: ConfirmOptions) {
-                  const preInstruction = await getPriorityFeePreInstruction();
-                  target['preInstructions'](preInstruction);
-                  return await target[prop](options);
-                };
-              }
-              return target[prop as keyof typeof target];
-            },
-          });
-          return methodProxy;
-        };
+        const value = target[prop as keyof typeof target];
+        if (value instanceof Function) {
+          return function (...args: unknown[]) {
+            const method = value(...args);
+            const methodProxy = new Proxy(method, {
+              get(target, prop) {
+                if (prop === 'rpc') {
+                  return async function (
+                    options?: ConfirmOptions & {
+                      disablePrioFees?: boolean;
+                    },
+                  ) {
+                    if (options?.disablePrioFees !== false) {
+                      const preInstruction =
+                        await getPriorityFeePreInstruction();
+                      target['preInstructions'](preInstruction);
+                    }
+                    return await target[prop](options);
+                  };
+                }
+                return target[prop as keyof typeof target];
+              },
+            });
+            return methodProxy;
+          };
+        }
+        return value;
       },
     });
   }
