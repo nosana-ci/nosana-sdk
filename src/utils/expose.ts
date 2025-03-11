9da50e66ab19fb846e4804499ff6ed8968edcf74
danieltosinfayemi <danieltosinfayemi@gmail.com>
2025-03-11 09:03:18 -0400

    feat: change crypto to node crypto

--- src/utils/expose.ts
+++ src/utils/expose.ts
@@ -1,4 +1,4 @@
-import { createHash } from "crypto";
+import { createHash } from "node:crypto";
 import { ExposedPort, JobDefinition, Operation, OperationArgsMap, OperationType } from "../types";
 
 const isPrivate = (job: JobDefinition): boolean => {
