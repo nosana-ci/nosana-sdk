263246f0ddae3e9e029969e93ccfed4834f1d5c2
KeesGeerligs <166137421+KeesGeerligs@users.noreply.github.com>
2025-09-15 16:34:08 +0200

    Stop group on operation finalization

--- src/types/job.ts
+++ src/types/job.ts
@@ -231,6 +231,7 @@ export type Operation<T extends OperationType> = {
 export type Execution = {
   group?: string;
   depends_on?: string[];
+  stop_group_on?: 'finish';
 };
 
 export interface OperationArgsMap {
