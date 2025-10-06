eb3a18ab7637ef918a67099e5c9b844091df06fd
KeesGeerligs <166137421+KeesGeerligs@users.noreply.github.com>
2025-10-06 10:55:30 +0200

    Remove the filter as its parsed before

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,4 @@ export * from './client.js';
 export * from './services/index.js';
 export * from './types/index.js';
 export * from './utils/expose.js';
-export * from './utils/resources.js';
 export * from './utils.js';
