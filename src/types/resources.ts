1b1022a0aedd66a16baeed1b05479aa9494f1575
Matthew Hammond <ultramatt96@hotmail.co.uk>
2025-10-23 08:34:30 +0000

    make remote resources more strict

--- src/types/resources.ts
+++ src/types/resources.ts
@@ -1,18 +1,37 @@
 type S3Base = {
   type: 'S3';
-  url?: string;
   target: string;
-  files?: string[];
+  url?: string;
   allowWrite?: boolean;
+  files?: string[]; // allow files in base
   IAM?: S3Auth;
-  bucket?: never;
-  buckets?: never;
+  // No bucket, no buckets
 };
 
-type S3WithBucket = S3Base & { bucket: string; buckets?: never };
-type S3WithBuckets = S3Base & { buckets: { url: string; files?: string[] }[]; bucket?: never };
+type S3WithBucket = {
+  type: 'S3';
+  target: string;
+  bucket: string;
+  url?: string;
+  allowWrite?: boolean;
+  IAM?: S3Auth;
+  // No buckets, no files
+};
 
-export type S3Unsecure = S3Base | S3WithBucket | S3WithBuckets;
+type S3WithBuckets = {
+  type: 'S3';
+  target: string;
+  buckets: { url: string; files?: string[] }[];
+  url?: string;
+  allowWrite?: boolean;
+  IAM?: S3Auth;
+  // No bucket, no files
+};
+
+export type S3Unsecure =
+  | S3Base
+  | S3WithBucket
+  | S3WithBuckets;
 
 export type S3Auth = {
   REGION: string;
@@ -25,22 +44,22 @@ export type ResourceBase = {
   target: string;
 };
 
-export type HFResource = ResourceBase & {
+export type HFResource = {
   type: 'HF';
+  target: string;
   repo: string;
   revision?: string;
   files?: string[];
   accessToken?: string;
 };
 
-export type OllamaResource = Partial<ResourceBase> & {
+export type OllamaResource = {
   type: 'Ollama';
   model: string;
-  target?: string
+  target?: string;
 };
 
-export type S3Resource = ResourceBase & S3Unsecure;
-
+export type S3Resource = S3Unsecure;
 export type Resource = S3Resource | HFResource | OllamaResource;
 
 export type RequiredResource = Omit<Resource, 'target'>;
